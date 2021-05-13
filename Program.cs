27374267757a0bf8383d31e0afe82178d77ac7a2
William Liebenberg <awliebenberg@outlook.com>
2021-05-13 22:59:04 +1000

    add IsAzureAppService extension method fix logic to determine credential provider

--- Program.cs
+++ Program.cs
@@ -9,6 +9,15 @@ using System;
 
 namespace KeyVaultExample
 {
+	public static class IWebHostEnvironmentExtensions
+	{
+		public static bool IsAzureAppService(this IWebHostEnvironment env)
+		{
+			var websiteName = Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME");
+			return string.IsNullOrEmpty(websiteName) is not true;
+		}
+	}
+
 	public class Program
 	{
 		public static void Main(string[] args)
@@ -24,13 +33,23 @@ namespace KeyVaultExample
 						.UseStartup<Startup>()
 						.ConfigureAppConfiguration((context, config) =>
 						{
+							// To run the "Production" app locally, modify your launchSettings.json file
+							// -> set ASPNETCORE_ENVIRONMENT value as "Production"
 							if (context.HostingEnvironment.IsProduction())
 							{
 								IConfigurationRoot builtConfig = config.Build();
 
-								// If running as "Production" from our local environment (not in Azure), then use the Azure CLI Credential provider. This means you
-								// have to log in via `az login` before running the local app as Production.
-								TokenCredential cred = context.HostingEnvironment.IsProduction() ? new DefaultAzureCredential(false) : new AzureCliCredential();
+								// ATTENTION:
+								//
+								// If running the app from your local dev machine (not in Azure AppService),
+								// -> use the AzureCliCredential provider.
+								// -> This means you have to log in locally via `az login` before running the app on your local machine.
+								//
+								// If running the app from Azure AppService
+								// -> use the DefaultAzureCredential provider
+								//
+								TokenCredential cred = context.HostingEnvironment.IsAzureAppService() ?
+									new DefaultAzureCredential(false) : new AzureCliCredential();
 
 								var keyvaultUri = new Uri($"https://{builtConfig["KeyVaultName"]}.vault.azure.net/");
 								var secretClient = new SecretClient(keyvaultUri, cred);
--- Startup.cs
+++ Startup.cs
@@ -6,45 +6,45 @@ using Microsoft.OpenApi.Models;
 
 namespace KeyVaultExample
 {
-
 	public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
 			services.Configure<ApplicationSecrets>(Configuration.GetSection("ApplicationSecrets"));
 
 			services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "KeyVaultEample", Version = "v1" });
-            });
-        }
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "KeyVaultEample", Version = "v1" });
+			});
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "KeyVaultEample v1"));
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
 
-            app.UseHttpsRedirection();
+			app.UseDeveloperExceptionPage();
+			app.UseSwagger();
+			app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "KeyVaultEample v1"));
 
-            app.UseRouting();
+			app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+			app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
 }
